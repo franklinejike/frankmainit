2a272c213f074285fd532219fe8aba6cbe42e576
Franklin <franklinnwekpa@gmail.com>
2025-02-17 14:36:51 +0100

    adding js to other pages

---
+++ contactus.js
@@ -0,0 +1,9 @@
+function displayNav(){
+    let mobileNav = document.querySelector(".mobileNav")
+    mobileNav.style.display= "block"
+}
+
+function removeNav(){
+    let mobileNav = document.querySelector(".mobileNav")
+    mobileNav.style.display= "none"
+}
